a77183af69e82d06675abaaafece3d9895155b82
Joshua Ssemwanga <jssem001@plattsburgh.edu>
2024-03-19 12:06:30 +0300

    reviewing control flow

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(f){
   // Write your code here!
+  let greeting;
+  if(f<=400){
+    greeting ='This one is on me!';
+    
+  }
+  if(f>400 && f<=2000){
+    greeting = 'That will be twenty bucks.';
+  }
+  if(f>2000 && f<2500){
+    greeting = 'I will gladly take your thirty bucks.';
+  }
+  if(f>2500){
+    greeting = 'No can do.';
+  }
+  return greeting;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let result;
+  if(city=='NYC'){
+    result = 'Ok, sounds good.';
+  }else{
+    result = 'No go.';
+  }
+  return result;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let response;
+  if(tip=='generous'){
+    response = 'Thank you so much.';
+  }
+  else if(tip=='not as generous'){
+    response = 'Thank you.';
+  }else{
+    response = 'Bye.';
+  }
+  return response;
 }
